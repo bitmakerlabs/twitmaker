1ed586d4a6706aba45b7bce317a7ae0bc5295068
Mina Mikhail <mina@fightingtheboss.com>
2015-12-01 17:54:08 -0500

    Add TweetsController to project

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  resources :tweets, only: [:index, :create, :destroy]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
