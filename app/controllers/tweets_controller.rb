81b93eeaaabb42e4d2b05270bed77eab541f93bb
Mina Mikhail <mina@fightingtheboss.com>
2015-12-01 18:16:44 -0500

    Define create method for tweets and add basic layout

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,10 +1,25 @@
 class TweetsController < ApplicationController
   def index
+    @tweets = Tweet.all
+    @tweet = Tweet.new
   end
 
   def create
+    @tweet = Tweet.new(tweet_params)
+
+    if @tweet.save
+      redirect_to tweets_path
+    else
+      render :index
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:message)
   end
 end
