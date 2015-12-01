e422ee4c161d87f48ab1afda38987c11c170e8a7
Mina Mikhail <mina@fightingtheboss.com>
2015-12-01 17:56:49 -0500

    Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+  root 'tweets#index'
   resources :tweets, only: [:index, :create, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
