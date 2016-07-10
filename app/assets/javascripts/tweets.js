2fb9808932508b85410bc5d9cea9c8f1694ba036
Mitch Pateman <mitch_pateman@hotmail.com>
2016-07-10 15:33:33 -0400

    Finished the post request section, and prepend to the top

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -1,2 +1,17 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(function(){   //doc ready
+	$('#new_tweet').on('submit', function(event){
+		event.preventDefault();
+
+		$.ajax({
+				url: $(this).attr('action'),
+				method: 'POST',
+				data: $(this).serialize(),
+				dataType: 'html'
+			}).done(function(responseData){
+				//console.log(responseData);
+				$(responseData).prependTo('.tweets');
+			});
+	});// end of on submit
+}); //end doc ready
