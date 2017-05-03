832b355895769c69cb58a1bbb5e07fac3f1e4a98
Mark <markjconlon@gmail.com>
2017-05-03 15:41:20 -0400

    part 1 working

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -1,2 +1,21 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(function(){
+  $('#new_tweet').on('submit', function(event){
+    event.preventDefault();
+    console.log($ (this).serialize()  );
+
+    $.ajax({
+      url: $(this).attr('action'),
+      method: $(this).attr('method'),
+      data: $(this).serialize(),
+      dataType: 'html'
+    }).done(function(data) {
+      debugger;
+      console.log('done');
+      $('.tweets').prepend(data);
+    }).fail(function(){
+      console.log('FAILED');
+    });
+  });
+});
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -7,10 +7,26 @@ class TweetsController < ApplicationController
   def create
     @tweet = Tweet.new(tweet_params)
 
-    if @tweet.save
-      redirect_to tweets_path
-    else
-      render :index
+    respond_to do |format|
+      format.html do
+        if request.xhr?
+          if @tweet.save
+            render partial: 'tweet', locals: { tweet: @tweet }
+          end
+        else
+          if @tweet.save
+            redirect_to tweets_path
+          else
+            @tweets = Tweets.all.order(created_at: :desc)
+            render :index
+          end
+        end
+      end
+      format.json do
+        if @tweet.save
+          render json: @tweet
+        end
+      end
     end
   end
 
