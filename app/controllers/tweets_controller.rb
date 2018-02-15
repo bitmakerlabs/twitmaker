2d3bf126bcb9902211c875818e2213dd7698503e
John Carlo Lopez <condottiere.jc@gmail.com>
2018-02-15 17:15:37 -0500

    complete assignment for both HTML and JSON response

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,13 +2,26 @@ class TweetsController < ApplicationController
   def index
     @tweets = Tweet.all.order(created_at: :desc)
     @tweet = Tweet.new
+
   end
 
   def create
     @tweet = Tweet.new(tweet_params)
 
     if @tweet.save
-      redirect_to tweets_path
+      respond_to do |format|
+        format.html {
+          if request.xhr?
+            puts '*'*20
+            p @tweet
+            render partial: 'tweet'
+          end
+          redirect_to tweets_path
+        }
+        format.json {
+          render json: {tweets: {message: @tweet.message, created_at: @tweet.created_at.strftime('%b %e, %l:%M %p')}}
+        }
+      end
     else
       render :index
     end
